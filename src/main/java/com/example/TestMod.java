b44e81842b99642f38c9cbbba56dd6ffc522e6c0
m1919810 <matl040527@qq.com>
2024-04-30 01:27:24 +0800

    test

--- src/main/java/com/example/TestMod.java
+++ src/main/java/com/example/TestMod.java
@@ -17,7 +17,7 @@ public class TestMod implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		System.out.println("BYDER");
 		LOGGER.info("Hello Fabric world!");
 	}
 }
