e18cfb25fa8dc75da711bedace498947a37946b4
m1919810 <matl040527@qq.com>
2025-03-14 16:37:16 +0800

    save progress

--- src/main/java/me/matl114/ManageUtils/HotKeys.java
+++ src/main/java/me/matl114/ManageUtils/HotKeys.java
@@ -200,6 +200,10 @@ public class HotKeys {
                 Tasks.doHokeyTaskTest1();
                 return true;
             }));
+            getTaskHotKey(OPEN_MENU,(manager->{
+                InvTasks.openSelectScreen();
+                return true;
+            }));
             getTaskHotKey(QUICK_DROP,(iInputManager -> InvTasks.dropAllSelectedItem()));
             getTaskHotKey(FAST_MOVE,(iInputManager -> InvTasks.quickMoveAllSelectedItem()));
             getTaskHotKey(FAST_DROP,(iInputManager -> InvTasks.quickDropAllSelectedItem()));
