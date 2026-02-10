d977695b396d47d282138f81eb80cffd5c7c1398
m1919810 <matl040527@qq.com>
2026-02-11 03:58:36 +0800

    spotless

--- src/main/java/me/matl114/bukkit/BukkitConfigDeserializor.java
+++ src/main/java/me/matl114/bukkit/BukkitConfigDeserializor.java
@@ -1,32 +1,36 @@
 package me.matl114.bukkit;
 
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
 import me.matl114.utils.Debug;
 import me.matl114.utils.ItemStackUtils;
 import me.matl114.versioned.api.VNbt;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.*;
-import java.util.List;
-import java.util.Map;
-import java.util.regex.Pattern;
 
 public class BukkitConfigDeserializor {
     private static final Pattern ARRAY = Pattern.compile("^\\[.*]");
     private static final Pattern INTEGER = Pattern.compile("[-+]?(?:0|[1-9][0-9]*)?i", Pattern.CASE_INSENSITIVE);
-    private static final Pattern DOUBLE = Pattern.compile("[-+]?(?:[0-9]+[.]?|[0-9]*[.][0-9]+)(?:e[-+]?[0-9]+)?d", Pattern.CASE_INSENSITIVE);
+    private static final Pattern DOUBLE =
+            Pattern.compile("[-+]?(?:[0-9]+[.]?|[0-9]*[.][0-9]+)(?:e[-+]?[0-9]+)?d", Pattern.CASE_INSENSITIVE);
 
     public static NbtElement deserializeObject(final Object object) {
         // The new logic expects the top level object to be a single string, holding the entire nbt tag as SNBT.
         if (object instanceof final String snbtString) {
             try {
-                return StringNbtReader.fromOps(ItemStackUtils.registry().getOps(NbtOps.INSTANCE)).read(snbtString);
+                return StringNbtReader.fromOps(ItemStackUtils.registry().getOps(NbtOps.INSTANCE))
+                        .read(snbtString);
             } catch (final CommandSyntaxException e) {
                 throw new RuntimeException("Failed to deserialise nbt", e);
             }
-        } else { // Legacy logic is passed to the internal legacy deserialization that attempts to read the old format that *unsuccessfully* attempted to read/write nbt to a full yml tree.
+        } else { // Legacy logic is passed to the internal legacy deserialization that attempts to read the old format
+            // that *unsuccessfully* attempted to read/write nbt to a full yml tree.
             return deserializeObjectLegacy(object);
         }
     }
+
     public static NbtElement deserializeObjectLegacy(Object object) {
         if (object instanceof Map) {
             NbtCompound compound = new NbtCompound();
@@ -53,21 +57,24 @@ public class BukkitConfigDeserializor {
             if (ARRAY.matcher(string).matches()) {
 
                 return VNbt.getInstance().readNbt(string);
-            } else if (INTEGER.matcher(string).matches()) { //Read integers on our own
+            } else if (INTEGER.matcher(string).matches()) { // Read integers on our own
                 return NbtInt.of(Integer.parseInt(string.substring(0, string.length() - 1)));
             } else if (DOUBLE.matcher(string).matches()) {
                 return NbtDouble.of(Double.parseDouble(string.substring(0, string.length() - 1)));
             } else {
                 NbtElement nbtBase;
-                try{
-                    nbtBase = StringNbtReader.fromOps(ItemStackUtils.registry().getOps(NbtOps.INSTANCE)).read(string);
-                }catch (CommandSyntaxException e){
+                try {
+                    nbtBase = StringNbtReader.fromOps(ItemStackUtils.registry().getOps(NbtOps.INSTANCE))
+                            .read(string);
+                } catch (CommandSyntaxException e) {
                     throw new RuntimeException("Could not deserialize found element ", e);
                 }
-                if (nbtBase instanceof NbtInt(int va)) { // If this returns an integer, it did not use our method from above
-                    return NbtString.of(String.valueOf(va)); // It then is a string that was falsely read as an int
+                if (nbtBase instanceof NbtInt nit) { // If this returns an integer, it did not use our method from above
+                    return NbtString.of(
+                            String.valueOf(nit.value())); // It then is a string that was falsely read as an int
                 } else if (nbtBase instanceof NbtDouble) {
-                    return NbtString.of(String.valueOf(((NbtDouble) nbtBase).doubleValue())); // Doubles add "d" at the end
+                    return NbtString.of(
+                            String.valueOf(((NbtDouble) nbtBase).doubleValue())); // Doubles add "d" at the end
                 } else {
                     return nbtBase;
                 }
@@ -76,28 +83,29 @@ public class BukkitConfigDeserializor {
 
         throw new RuntimeException("Could not deserialize NBTBase");
     }
-    public static final String TEST_CASE = "item:\n" +
-        "  ==: org.bukkit.inventory.ItemStack\n" +
-        "  v: 3465\n" +
-        "  type: DIRT\n" +
-        "  meta:\n" +
-        "    ==: ItemMeta\n" +
-        "    meta-type: UNSPECIFIC\n" +
-        "    PublicBukkitValues:\n" +
-        "      infinityexpansion:display: 351372703i\n" ;
-    public static BukkitItemStack deserializeItemFromString(String string){
-        return deserializeItemFromStringTest(string);
 
+    public static final String TEST_CASE = "item:\n" + "  ==: org.bukkit.inventory.ItemStack\n"
+            + "  v: 3465\n"
+            + "  type: DIRT\n"
+            + "  meta:\n"
+            + "    ==: ItemMeta\n"
+            + "    meta-type: UNSPECIFIC\n"
+            + "    PublicBukkitValues:\n"
+            + "      infinityexpansion:display: 351372703i\n";
+
+    public static BukkitItemStack deserializeItemFromString(String string) {
+        return deserializeItemFromStringTest(string);
     }
-    public static BukkitItemStack deserializeItemFromStringTest(String string){
+
+    public static BukkitItemStack deserializeItemFromStringTest(String string) {
         BukkitYaml config = new BukkitYaml();
         try {
-            return config.getItemStackFromString(string);//  config.loadFromString(string);
+            return config.getItemStackFromString(string); //  config.loadFromString(string);
         } catch (BukkitYaml.InvalidConfigException var3) {
             Debug.info(var3);
-            return new BukkitItemStack(Items.STONE,1);
+            return new BukkitItemStack(Items.STONE, 1);
         }
-//        BukkitItemStack item = config.getObject("item",BukkitItemStack.class);
-//        return (item != null ? item :new BukkitItemStack(Items.STONE,1));
+        //        BukkitItemStack item = config.getObject("item",BukkitItemStack.class);
+        //        return (item != null ? item :new BukkitItemStack(Items.STONE,1));
     }
 }
