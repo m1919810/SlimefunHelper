cffe6387fb492af338b94d4f3955d6357c401abf
m1919810 <matl040527@qq.com>
2026-02-19 23:02:34 +0800

    feat: add fucking CE support

---
+++ src/stubs/java/com/jsmacrosce/jsmacros/client/api/library/impl/FJavaUtils.java
@@ -0,0 +1,13 @@
+package xyz.wagyourtail.jsmacros.client.api.library.impl;
+
+import org.jspecify.annotations.Nullable;
+
+import java.util.Objects;
+
+public class FJavaUtils {
+    @Nullable
+    public Object getHelperFromRaw(Object raw) {
+        Objects.requireNonNull(raw, "Object cannot be null.");
+        return null;
+    }
+}
