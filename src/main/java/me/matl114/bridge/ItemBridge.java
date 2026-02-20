f286ae79c74aed6dc99f296b1a85a78f89006df6
m1919810 <matl040527@qq.com>
2026-02-20 20:39:59 +0800

    feat: add item

--- src/main/java/me/matl114/bridge/ItemBridge.java
+++ src/main/java/me/matl114/bridge/ItemBridge.java
@@ -1,7 +1,19 @@
 package me.matl114.bridge;
 
+import com.mojang.authlib.properties.PropertyMap;
+import me.matl114.bukkit.BukkitItemStackUtils;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.component.type.ProfileComponent;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
 
 public class ItemBridge {
     public static void init() {}
@@ -9,6 +21,20 @@ public class ItemBridge {
     public static Item TESTITEM;
 
     static {
-        TESTITEM = Items.register("myitem", Item::new);
+        TESTITEM = Items.register("myitem", (settings -> {
+            Item item = new Item(
+                settings.component(DataComponentTypes.MAX_STACK_SIZE, 11)
+                    .component(DataComponentTypes.PROFILE, BukkitItemStackUtils.buildPlayerHeadProfileCSCoreLib("669c95e7451629add7845233131da5543157b8df52afadec1a586a0e549ee777"))
+                    .food(new FoodComponent(
+                        0,0, true
+                    )
+                )
+            );
+            if(item.getComponents() instanceof ComponentMap.Builder.SimpleComponentMap map0) {
+                map0.map().put(DataComponentTypes.ITEM_MODEL, Identifier.tryParse("minecraft:player_head"));
+                map0.map().put(DataComponentTypes.ITEM_NAME, Text.literal("zc_zyro的龟头"));
+            };
+            return item;
+        }));
     }
 }
