43ea8b3aff3571eab350cb156b351e8f897dfb1b
m1919810 <matl040527@qq.com>
2026-02-20 20:43:24 +0800

    feat: remove annotation

--- src/stubs/java/com/jsmacrosce/jsmacros/client/api/library/impl/FJavaUtils.java
+++ src/stubs/java/com/jsmacrosce/jsmacros/client/api/library/impl/FJavaUtils.java
@@ -1,10 +1,9 @@
 package com.jsmacrosce.jsmacros.client.api.library.impl;
 
 import java.util.Objects;
-import org.jspecify.annotations.Nullable;
 
 public class FJavaUtils {
-    @Nullable
+
     public Object getHelperFromRaw(Object raw) {
         Objects.requireNonNull(raw, "Object cannot be null.");
         return null;
