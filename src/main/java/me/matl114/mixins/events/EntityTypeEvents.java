77594d8d53b7e225797bf551be19f49a3823a980
m1919810 <matl040527@qq.com>
2026-03-06 01:34:22 +0800

    fix: version methods

--- src/main/java/me/matl114/mixins/events/EntityTypeEvents.java
+++ src/main/java/me/matl114/mixins/events/EntityTypeEvents.java
@@ -9,7 +9,6 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnReason;
 import net.minecraft.world.World;
-import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -23,7 +22,7 @@ public abstract class EntityTypeEvents {
                             value = "INVOKE",
                             target =
                                     "Lnet/minecraft/entity/EntityType$EntityFactory;create(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;)Lnet/minecraft/entity/Entity;"))
-    private <T extends Entity> @Nullable T onCreate(
+    private <T extends Entity> T onCreate(
             EntityType.EntityFactory<T> instance,
             EntityType<T> tEntityType,
             World world,
