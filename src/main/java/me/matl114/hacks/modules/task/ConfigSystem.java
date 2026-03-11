3e56b1b007ac3f86d8d3305a6301ae5a907dd421
m1919810 <matl040527@qq.com>
2026-03-12 07:49:27 +0800

    fix: config button

--- src/main/java/me/matl114/hacks/modules/task/ConfigSystem.java
+++ src/main/java/me/matl114/hacks/modules/task/ConfigSystem.java
@@ -1,5 +1,9 @@
 package me.matl114.hacks.modules.task;
 
+import me.matl114.accessors.gui.ScreenAccess;
+import me.matl114.events.Event;
+import me.matl114.events.Listener;
+import me.matl114.gui.basic.*;
 import me.matl114.hacks.MainTasks;
 import me.matl114.hacks.api.BaseModule;
 import me.matl114.managers.Configs;
@@ -8,6 +12,10 @@ import me.matl114.managers.config.KeyBindRef;
 import me.matl114.managers.input.HotKeyUtils;
 import me.matl114.managers.input.KeyCode;
 import me.matl114.managers.input.MultiKeyBind;
+import me.matl114.utils.collections.Point;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
+import net.minecraft.text.Text;
 
 public class ConfigSystem extends BaseModule {
     public static final String[] OPEN_MENU_HOTKEY = new String[] {"hotkeys", "open-menu"};
@@ -19,11 +27,43 @@ public class ConfigSystem extends BaseModule {
             .registerHotkey(HotKeyUtils.wrapAsHandler(this::openConfigMenu))
             .build();
 
+    @Override
+    public void registerAll() {
+        super.registerAll();
+        registerListener(Listener.getPostInitializeScreen(), this::onScreenInitialize);
+        registerListener(Listener.getResolutionChange(), this::onResolutionChange);
+    }
+
     public void openConfigMenu() {
         MainTasks.openConfigNewStyleScreen();
     }
 
     public void openConfigScreen(Config config) {
         MainTasks.openConfigScreen(config);
+    }
+
+    private ContentDelegateWidget<ExecutableWidget> delegateWidget = new ContentDelegateWidget<>(0, 0, 100, 20);
+
+    public void onScreenInitialize(Event<Screen> screenEvent) {
+        if (screenEvent.context() instanceof MultiplayerScreen mp) {
+            ExecutableWidget executableWidget = ExecutableWidget.instance(0, 0, 100, 20)
+                    .setElementHandler(new ButtonElement(
+                            TextProvider.of(Text.literal("SlimefunHelper")),
+                            ButtonAction.run(() -> MainTasks.getConfigSystem().openConfigScreen(Configs.HTTP_CONFIG))));
+            delegateWidget.setContentDelegate(executableWidget);
+            delegateWidget.addTo(mp);
+            delegateWidget.setX(mp.width - 205);
+            delegateWidget.setY(5);
+            ScreenAccess.of(mp).removeChildFrom(delegateWidget);
+            ScreenAccess.of(mp).addDrawableChildTo(delegateWidget);
+        }
+    }
+
+    public void onResolutionChange(Event<Point> resolutionChange) {
+        if (mc.currentScreen instanceof MultiplayerScreen) {
+            int x = resolutionChange.context().x;
+            delegateWidget.setX(x - 205);
+            delegateWidget.setY(5);
+        }
     }
 }
