0f57ce13e44c0948f66a41b5ee6274aae472029c
m1919810 <matl040527@qq.com>
2026-06-12 23:10:59 +0800

    feat: save progress

---
+++ src/main/java/me/matl114/hacks/utils/config/PrimitivePairList.java
@@ -0,0 +1,146 @@
+package me.matl114.hacks.utils.config;
+
+import com.google.common.base.Preconditions;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import me.matl114.managers.config.NBTParsable;
+import me.matl114.managers.config.NBTType;
+import me.matl114.utils.config.AttrKeyValue;
+import me.matl114.utils.config.PairLikeFactory;
+import me.matl114.utils.config.WrapperFactory;
+import me.matl114.utils.config.kv.AttrKeyValues;
+import me.matl114.utils.config.kv.TypeConvertAttrKeyValue;
+
+@Getter
+@Accessors(fluent = true)
+public class PrimitivePairList<T, W> implements NBTParsable<PrimitivePairList<T, W>> {
+    final NBTType<T> firstType;
+    final NBTType<W> secondType;
+    final List<Pair<T, W>> list;
+    List<Pair<Primitive<T>, Primitive<W>>> originValue;
+
+    public PrimitivePairList(NBTType<T> firstType, NBTType<W> secondType, List<Pair<T, W>> list) {
+        this.firstType = firstType;
+        this.secondType = secondType;
+        this.list = new ArrayList<>(list);
+    }
+
+    public PrimitivePairList(List<Pair<Primitive<T>, Primitive<W>>> primitiveList, NBTType<T> firstType, NBTType<W> secondType) {
+        this.firstType = firstType;
+        this.secondType = secondType;
+        this.originValue = primitiveList;
+        this.list = new ArrayList<>(primitiveList.size());
+        for (var entry : primitiveList) {
+            Primitive<T> first = entry.getFirst();
+            Primitive<W> second = entry.getSecond();
+            Preconditions.checkArgument(first.valueType() == firstType);
+            Preconditions.checkArgument(second.valueType() == secondType);
+            this.list.add(Pair.of(first.value(), second.value()));
+        }
+    }
+
+    public List<Pair<Primitive<T>, Primitive<W>>> toPrimitivePairList() {
+        if (originValue == null) {
+            List<Pair<Primitive<T>, Primitive<W>>> cached = new ArrayList<>();
+            for (var entry : list) {
+                cached.add(Pair.of(
+                        Primitive.of(firstType, entry.getFirst()),
+                        Primitive.of(secondType, entry.getSecond())));
+            }
+            this.originValue = cached;
+        }
+        return originValue;
+    }
+
+    NBTType<List<Pair<Primitive<T>, Primitive<W>>>> cachedEntryType;
+
+    public static final <T, W> NBTType<PrimitivePairList<T, W>> create() {
+        Codec<Pair<Primitive<T>, Primitive<W>>> pairCodec = NBTTypes.createPairLike(
+                        Pair.class,
+                        Primitive.TYPE.<Primitive<T>>cast(),
+                        "first",
+                        Primitive.TYPE.<Primitive<W>>cast(),
+                        "second",
+                        PairLikeFactory.of(Pair::of, Pair::getFirst, Pair::getSecond),
+                        AttrKeyValue.CustomWidgetFactory.cutSizeXLeft(0.5),
+                        AttrKeyValue.CustomWidgetFactory.cutSizeXRight(0.5))
+                .typeCodec();
+        return new NBTType<PrimitivePairList<T, W>>(
+                NBTType.<PrimitivePairList<T, W>>parameter(PrimitivePairList.class),
+                RecordCodecBuilder.<PrimitivePairList<T, W>>create(instance -> instance
+                        .group(
+                                Codec.list(pairCodec)
+                                        .fieldOf("data")
+                                        .forGetter(PrimitivePairList::toPrimitivePairList),
+                                NBTTypes.<T>codec().fieldOf("first_type").forGetter(PrimitivePairList::firstType),
+                                NBTTypes.<W>codec().fieldOf("second_type").forGetter(PrimitivePairList::secondType))
+                        .apply(instance, PrimitivePairList::new)),
+                (AttrKeyValue.CustomWidgetFactory<PrimitivePairList<T, W>>) (attr, x, y, dx, dy) -> {
+                    PrimitivePairList<T, W> pairList = attr.getOriginValue();
+                    NBTType<Pair<Primitive<T>, Primitive<W>>> pairType = NBTTypes.createPairLike(
+                            Pair.class,
+                            Primitive.TYPE.<Primitive<T>>cast(),
+                            "first",
+                            Primitive.TYPE.<Primitive<W>>cast(),
+                            "second",
+                            PairLikeFactory.of(Pair::of, Pair::getFirst, Pair::getSecond),
+                            AttrKeyValue.CustomWidgetFactory.cutSizeXLeft(0.5),
+                            AttrKeyValue.CustomWidgetFactory.cutSizeXRight(0.5));
+                    AttrKeyValue.CustomWidgetFactory<List<Pair<Primitive<T>, Primitive<W>>>> widgetFactory = (w1, x1, y1, dx1, dy1) -> {
+                        return NBTTypes.generateListModifyButton(
+                                w1,
+                                pairType,
+                                () -> Pair.of(
+                                        Primitive.of(pairList.firstType, pairList.firstType.createEmpty()),
+                                        Primitive.of(pairList.secondType, pairList.secondType.createEmpty())),
+                                x1,
+                                y1,
+                                dx1,
+                                dy1,
+                                300,
+                                20);
+                    };
+                    WrapperFactory<String, List<Pair<Primitive<T>, Primitive<W>>>> stringListWrapperFactory =
+                            AttrKeyValues.STR_LIST_FACTORY.concat(WrapperFactory.list(pairType.stringifyFactory()));
+                    WrapperFactory<List<Pair<Primitive<T>, Primitive<W>>>, PrimitivePairList<T, W>> wrapperFactory =
+                            WrapperFactory.of(
+                                    mp -> new PrimitivePairList<>(mp, pairList.firstType, pairList.secondType),
+                                    PrimitivePairList::toPrimitivePairList);
+                    return new TypeConvertAttrKeyValue<>(attr, wrapperFactory, widgetFactory, stringListWrapperFactory)
+                            .generateValueWidget(x, y, dx, dy);
+                },
+                null,
+                (PrimitivePairList<T, W>) new PrimitivePairList<>(NBTTypes.STRING_TYPE, NBTTypes.STRING_TYPE, List.of()));
+    }
+
+    public static final NBTType<PrimitivePairList<Object, Object>> TYPE = create();
+
+    public static <T, W> Class<PrimitivePairList<T, W>> parameter() {
+        return (Class<PrimitivePairList<T, W>>) (Class) PrimitivePairList.class;
+    }
+
+    @Override
+    public NBTType<PrimitivePairList<T, W>> type() {
+        return TYPE.cast();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof PrimitivePairList<?, ?> that)) return false;
+        return Objects.equals(firstType, that.firstType)
+                && Objects.equals(secondType, that.secondType)
+                && Objects.equals(list, that.list);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstType, secondType, list);
+    }
+}
---
+++ src/main/java/me/matl114/hacks/utils/config/UnboundPrimitiveMap.java
@@ -0,0 +1,131 @@
+package me.matl114.hacks.utils.config;
+
+import com.google.common.base.Preconditions;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+import lombok.Getter;
+import lombok.experimental.Accessors;
+import me.matl114.managers.config.NBTParsable;
+import me.matl114.managers.config.NBTType;
+import me.matl114.utils.CodecUtils;
+import me.matl114.utils.config.AttrKeyValue;
+import me.matl114.utils.config.WrapperFactory;
+import me.matl114.utils.config.kv.TypeConvertAttrKeyValue;
+
+@Getter
+@Accessors(fluent = true)
+public class UnboundPrimitiveMap<T, W> implements NBTParsable<UnboundPrimitiveMap<T, W>> {
+    final NBTType<T> keyType;
+    final NBTType<W> valueType;
+    final Map<T, W> map;
+    Map<Primitive<T>, Primitive<W>> originValue;
+
+    public UnboundPrimitiveMap(NBTType<T> keyType, NBTType<W> valueType, Map<T, W> map) {
+        this.keyType = keyType;
+        this.valueType = valueType;
+        this.map = new LinkedHashMap<>(map);
+    }
+
+    public UnboundPrimitiveMap(Map<Primitive<T>, Primitive<W>> map, NBTType<T> keyType, NBTType<W> valueType) {
+        this.keyType = keyType;
+        this.valueType = valueType;
+        this.originValue = map;
+        this.map = new LinkedHashMap<>(map.size());
+        for (Map.Entry<Primitive<T>, Primitive<W>> entry : map.entrySet()) {
+            Primitive<T> key = entry.getKey();
+            Primitive<W> value = entry.getValue();
+            Preconditions.checkArgument(key.valueType() == keyType);
+            Preconditions.checkArgument(value.valueType() == valueType);
+            this.map.put(key.value(), value.value());
+        }
+    }
+
+    public Map<Primitive<T>, Primitive<W>> toPrimitiveMap() {
+        if (originValue == null) {
+            Map<Primitive<T>, Primitive<W>> cached = new LinkedHashMap<>();
+            for (var entry : map.entrySet()) {
+                cached.put(Primitive.of(keyType, entry.getKey()), Primitive.of(valueType, entry.getValue()));
+            }
+            this.originValue = cached;
+        }
+        return originValue;
+    }
+
+    NBTType<Map<Primitive<T>, Primitive<W>>> cachedEntryType;
+
+    public static final <T, W> NBTType<UnboundPrimitiveMap<T, W>> create() {
+        Codec<Primitive<T>> keyCodec = Primitive.TYPE.<Primitive<T>>cast().typeCodec();
+        Codec<Primitive<W>> valueCodec = Primitive.TYPE.<Primitive<W>>cast().typeCodec();
+        Function<UnboundPrimitiveMap<T, W>, NBTType<Map<Primitive<T>, Primitive<W>>>> typeGenerator = (w) -> {
+            if (w.cachedEntryType == null) {
+                w.cachedEntryType = NBTTypes.createArrayMapLike(
+                        "unbound_primitive_map",
+                        Primitive.TYPE.<Primitive<T>>cast(),
+                        () -> Primitive.of(w.keyType, w.keyType.createEmpty()),
+                        "key",
+                        Primitive.TYPE.<Primitive<W>>cast(),
+                        () -> Primitive.of(w.valueType, w.valueType.createEmpty()),
+                        "value",
+                        WrapperFactory.identity(),
+                        AttrKeyValue.CustomWidgetFactory.cutSizeXLeft(0.5),
+                        AttrKeyValue.CustomWidgetFactory.cutSizeXRight(0.5),
+                        300,
+                        20);
+            }
+            return w.cachedEntryType;
+        };
+        return new NBTType<UnboundPrimitiveMap<T, W>>(
+                NBTType.<UnboundPrimitiveMap<T, W>>parameter(UnboundPrimitiveMap.class),
+                RecordCodecBuilder.<UnboundPrimitiveMap<T, W>>create(instance -> instance
+                        .group(
+                                CodecUtils.arrayMapCodec(keyCodec, valueCodec)
+                                        .fieldOf("data")
+                                        .forGetter(UnboundPrimitiveMap::toPrimitiveMap),
+                                NBTTypes.<T>codec().fieldOf("key_type").forGetter(UnboundPrimitiveMap::keyType),
+                                NBTTypes.<W>codec().fieldOf("value_type").forGetter(UnboundPrimitiveMap::valueType))
+                        .apply(instance, UnboundPrimitiveMap::new)),
+                (attr, x, y, dx, dy) -> {
+                    UnboundPrimitiveMap<T, W> map = attr.getOriginValue();
+                    WrapperFactory<Map<Primitive<T>, Primitive<W>>, UnboundPrimitiveMap<T, W>> wrapperFactory =
+                            WrapperFactory.of(mp -> new UnboundPrimitiveMap<>(mp, map.keyType, map.valueType),
+                                    UnboundPrimitiveMap::toPrimitiveMap);
+                    return new TypeConvertAttrKeyValue<>(attr, wrapperFactory, typeGenerator.apply(map))
+                            .generateValueWidget(x, y, dx, dy);
+                },
+                null,
+                (UnboundPrimitiveMap<T, W>) new UnboundPrimitiveMap<>(NBTTypes.STRING_TYPE, NBTTypes.STRING_TYPE, Map.of()));
+    }
+
+    public static final NBTType<UnboundPrimitiveMap<Object, Object>> TYPE = create();
+
+    public static <T, W> Class<UnboundPrimitiveMap<T, W>> parameter() {
+        return (Class<UnboundPrimitiveMap<T, W>>) (Class) UnboundPrimitiveMap.class;
+    }
+
+    public W get(T key) {
+        return map.get(key);
+    }
+
+    @Override
+    public NBTType<UnboundPrimitiveMap<T, W>> type() {
+        return TYPE.cast();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (!(object instanceof UnboundPrimitiveMap<?, ?> that)) return false;
+        return Objects.equals(keyType, that.keyType)
+                && Objects.equals(valueType, that.valueType)
+                && Objects.equals(map, that.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(keyType, valueType, map);
+    }
+}
