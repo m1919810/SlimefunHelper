ee13683f9068d43798f00ebf95b55badefc35a94
m1919810 <matl040527@qq.com>
2026-06-29 02:32:06 +0800

    compat 1.21.4

--- src/main/java/me/matl114/versioned/impl/DrawContext_v1_21_1.java
+++ src/main/java/me/matl114/versioned/impl/DrawContext_v1_21_1.java
@@ -250,6 +250,11 @@ public class DrawContext_v1_21_1 implements VDrawContext {
         this.drawContext.fill(x1, y1, x2, y2, z, color);
     }
 
+    @Override
+    public void lineGuiGradient(int x1, int y1, int x2, int y2, int color1, int color2, int depth) {
+
+    }
+
     private void addInternal(Runnable runnable) {
         if (this.delayedDrawing != null) {
             final Runnable prev = this.delayedDrawing;
