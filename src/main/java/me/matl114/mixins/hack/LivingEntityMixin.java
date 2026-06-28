7ef9525f321f1eee80b95e09968e1cc3f7cad7fc
m1919810 <matl040527@qq.com>
2026-06-29 02:34:41 +0800

    compat 1.21.4

--- src/main/java/me/matl114/mixins/hack/LivingEntityMixin.java
+++ src/main/java/me/matl114/mixins/hack/LivingEntityMixin.java
@@ -43,14 +43,14 @@ public abstract class LivingEntityMixin extends Entity implements LivingEntityAc
     @Shadow
     protected abstract float getJumpVelocity(float st);
 
-    @Shadow
-    protected abstract void travelGliding(Vec3d movementInput);
 
     @Shadow
     public abstract boolean isFallFlying();
 
     @Shadow
     public abstract void remove(RemovalReason reason);
+
+    @Shadow protected abstract void travelGliding();
 
     @Unique
     @Override
@@ -105,7 +105,7 @@ public abstract class LivingEntityMixin extends Entity implements LivingEntityAc
     private void onWaterGlide(Vec3d movementInput, CallbackInfo ci) {
         if (ElytraExtra.INSTANCE.ignoreLiquidPushFly.get() && isFallFlying()) {
             ci.cancel();
-            travelGliding(movementInput);
+            travelGliding();
         }
     }
 
