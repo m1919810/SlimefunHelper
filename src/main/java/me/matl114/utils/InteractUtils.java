900438692f0f5f2c5c50e226db31664be759d2b8
m1919810 <matl040527@qq.com>
2026-07-11 22:20:31 +0800

    bugfix: respawn anchor interact

--- src/main/java/me/matl114/utils/InteractUtils.java
+++ src/main/java/me/matl114/utils/InteractUtils.java
@@ -145,21 +145,11 @@ public class InteractUtils {
         return true;
     }
 
-    private static boolean isInteractableRespawnAnchor(World world, BlockState state, @Nullable PlayerEntity player) {
-        if (canRespawnAnchorExplode(world)) {
-            return true;
-        }
+    private static boolean isInteractableRespawnAnchor(BlockState state, ItemStack stack) {
         int charges = state.get(RespawnAnchorBlock.CHARGES);
-        if (player != null && charges < 4) {
-            if (player.getMainHandStack().isOf(Items.GLOWSTONE)
-                    || player.getOffHandStack().isOf(Items.GLOWSTONE)) {
-                return true;
-            }
-        }
-        if (charges == 0) {
+        if (charges == 0 && !stack.isOf(Items.GLOWSTONE)) {
             return false;
         }
-
         return true;
     }
 
@@ -207,7 +197,7 @@ public class InteractUtils {
             World world, PlayerEntity player, BlockPos pos, BlockState state, ItemStack interactStack) {
         Block block = state.getBlock();
         if (block instanceof RespawnAnchorBlock) {
-            return isInteractableRespawnAnchor(world, state, player);
+            return isInteractableRespawnAnchor(state, interactStack);
         }
         if (block instanceof LecternBlock) {
             return state.contains(LecternBlock.HAS_BOOK) && state.get(LecternBlock.HAS_BOOK);
