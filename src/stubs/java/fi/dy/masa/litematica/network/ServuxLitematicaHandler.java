83e0c95f8519a0c39a199195664d967709313ff4
m1919810 <matl040527@qq.com>
2026-07-11 22:02:09 +0800

    versioned code

--- src/stubs/java/fi/dy/masa/litematica/network/ServuxLitematicaHandler.java
+++ src/stubs/java/fi/dy/masa/litematica/network/ServuxLitematicaHandler.java
@@ -1,9 +1,8 @@
 package fi.dy.masa.litematica.network;
 
 import net.minecraft.nbt.NbtCompound;
-import org.jspecify.annotations.Nullable;
 
 public abstract class ServuxLitematicaHandler {
 
-    private void handleBulkData(final int type, @Nullable NbtCompound nbt) {}
+    private void handleBulkData(final int type, NbtCompound nbt) {}
 }
