a327c02cf28ed1bbdb079d67493e1199d70c5279
m1919810 <matl040527@qq.com>
2026-07-12 12:02:01 +0800

    feat: 1. new feat

--- src/main/java/me/matl114/mixins/events/LivingEntityEvents.java
+++ src/main/java/me/matl114/mixins/events/LivingEntityEvents.java
@@ -150,6 +150,7 @@ public abstract class LivingEntityEvents extends Entity
 
     @Unique
     public final void updateEquipmentAttributeChange() {
+        getClientLastEquipmentSnapshot();
         Map<EquipmentSlot, ItemStack> map = null;
         Iterator var2 = EquipmentSlot.VALUES.iterator();
         ItemStack itemStack2;
